bba4441f9eb1365d2e9ad3b3f417ab7f18e14ce0
Johan Gabriels <johan.gabriels@vikingco.com>
2018-04-17 21:38:48 +0200

    Control app via adb argument, no user actions.

--- app/src/main/java/com/jogazo/network/SimpleGet.java
+++ app/src/main/java/com/jogazo/network/SimpleGet.java
@@ -2,9 +2,7 @@ package com.jogazo.network;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 import android.app.DownloadManager;
 import android.content.Context;
 import android.net.Uri;
@@ -12,15 +10,15 @@ import android.util.Log;
 
 public class SimpleGet extends AppCompatActivity {
     private static final String LOGTAG = "NetworkExplorer";
-    DownloadManager downloadManager;
+    private DownloadManager downloadManager;
 
-    private EditText getInput;
-    private Button getButton;
-    private String inputter;
+    private TextView getOutput;
+    private String inputter = "";
+    private String fileName = "_";
 
 /*
     You can launch via:
-    am start -a android.intent.action.VIEW -n com.jogazo.network/com.jogazo.network.SimpleGet -e theurl "http://ipv4.download.thinkbroadband.com/100MB.zip"
+    am start -a android.intent.action.VIEW -n com.jogazo.network/com.jogazo.network.SimpleGet -e theurl "https://fredvdvd.github.io/test1Mb.db" -e filename "21h46.db"
 */
 
     @Override
@@ -28,50 +26,49 @@ public class SimpleGet extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_get);
 
-        this.getInput = (EditText) findViewById(R.id.get_input);
-        this.getButton = (Button) findViewById(R.id.get_button);
+        this.getOutput = (TextView) findViewById(R.id.get_output);
 
         if (getIntent().hasExtra("theurl")) {
             inputter = getIntent().getStringExtra("theurl");
             Log.i(LOGTAG, "Got extra argument: " + inputter);
+
+            getOutput.setText(inputter);
+
+            if (getIntent().hasExtra("filename")){
+                fileName = getIntent().getStringExtra("filename");
+                Log.i(LOGTAG, "Got filename argument: " + fileName);
+            }
         }
         else {
-            inputter = getInput.getText().toString();
-            Log.i(LOGTAG, "inputter: " + inputter);
+            Log.w(LOGTAG, "No valid URL was given!");
         }
 
         downloadManager = (DownloadManager) this.getSystemService(Context.DOWNLOAD_SERVICE);
         Log.d(LOGTAG, "I have downloadManager");
-
-        this.getButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                inputter = getInput.getText().toString();
-//                Log.i(LOGTAG, "inputter: " + inputter);
-                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(inputter));
-
-                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE);
-                request.setAllowedOverRoaming(false);
-                request.setTitle("thinkbroadband zip");
-                request.setDescription("Downloading " + "zip");
-                request.setVisibleInDownloadsUi(true);
-                request.setDestinationInExternalPublicDir("/Download/", "5MB.zip");
-
-                downloadManager.enqueue(request);
-            }
-        });
     }
 
-}
-
-/*
-    onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.xml_layout);
-        if(getIntent().hasExtra(yourKeyName)) {
-            stringData = getIntent().getStringExtra(yourKeyName);
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (inputter != "") {
+            doDownload(inputter, fileName);
         }
-        //Do other stuff
+        else {
+            Log.w(LOGTAG, "CANNOT DOWNLOAD!");
+            finish();
+        }
     }
 
-}*/
+    void doDownload(String url, String fileName){
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE);
+        request.setAllowedOverRoaming(true);
+        request.setTitle(fileName);
+        request.setDescription("Downloaded via adb am start");
+        request.setVisibleInDownloadsUi(true);
+        request.setDestinationInExternalPublicDir("/Download/", fileName);
+
+        downloadManager.enqueue(request);
+    }
+}
